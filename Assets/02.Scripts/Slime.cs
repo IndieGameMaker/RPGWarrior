edf91aa770aeb6c99fbfca2299f48a4db37c637d
Lee JaeHyun <myleje@gmail.com>
2021-01-26 21:04:22 +0900

    .

--- Assets/02.Scripts/Slime.cs
+++ Assets/02.Scripts/Slime.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    //슬라임 자신의 Transform
+    [HideInInspector]
+    public Transform tr;
+
     //추적해야할 목표물의 Transform (Mummy's Transform)
     public Transform targetTr;
 
     public float moveSpeed = 1.5f;
     public float turnSpeed = 200.0f;
     public float traceDist = 5.0f;
-    
-    //슬라임 자신의 Transform
-    [HideInInspector]
-    public Transform tr;
 
-    void Start()
+    private void Start()
     {
         tr = this.gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         //두 벡터간의 거리를 계산
-        float distance = Vector3.Distance(tr.position, targetTr.position);
+        var distance = Vector3.Distance(tr.position, targetTr.position);
 
         if (distance <= traceDist)
         {

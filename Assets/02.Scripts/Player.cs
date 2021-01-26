c2867a8b3e33c6e3e66d23490807d006ba337502
Lee JaeHyun <myleje@gmail.com>
2021-01-26 15:15:39 +0900

    .

--- Assets/02.Scripts/Player.cs
+++ Assets/02.Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
 {
     //전역 변수
     public Transform tr;
-    public float speed = 0.05f;
+    public float speed = 2.0f;
 
     void Start()
     {
@@ -17,6 +17,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        tr.position += new Vector3(0.0f, 0.0f, speed);
+        float v = Input.GetAxis("Vertical"); //Up, Down/ W, S  : (-1.0f ~ 0.0f ~ +1.0f)
+        Debug.Log($"v={v}");
+
+        tr.Translate(Vector3.forward * Time.deltaTime * speed); //Vector3(0, 0, 1) * speed
     }
 }

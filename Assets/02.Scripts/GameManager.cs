d5db154998b308fe07e61046a2a4e23c17cc0342
Lee JaeHyun <myleje@gmail.com>
2021-01-27 13:54:04 +0900

    .

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -7,10 +7,14 @@ public class GameManager : MonoBehaviour
     public GameObject slime1;
     public GameObject slime2;
 
+    public List<GameObject> npc = new List<GameObject>();
+
     void Start()
     {
-        slime1 = Resources.Load<GameObject>("Slime");  
-        slime2 = Resources.Load<GameObject>("Turtle");      
+        slime1 = Resources.Load<GameObject>("NPC/Slime");  
+        slime2 = Resources.Load<GameObject>("NPC/Turtle");   
+
+        npc.AddRange(Resources.LoadAll<GameObject>("NPC"));
     }
 
     // Update is called once per frame
